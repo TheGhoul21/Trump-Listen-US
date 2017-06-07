654a4ea6d82910e7d1abc65c30fe77f99136cb79
Luca Simonetti <little.che90@gmail.com>
2017-06-07 21:33:01 +0200

    fix: fix app.py port

--- app.py
+++ app.py
@@ -18,6 +18,5 @@ def static_file(path):
     return app.send_static_file(path)
 
 if __name__ == '__main__':
-  # db.create_all()
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port, debug=True, use_reloader=True)
