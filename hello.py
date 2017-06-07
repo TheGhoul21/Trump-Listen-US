d81d767b6b246ba31f1c7a596342d349fb9b70f1
Luca Simonetti <little.che90@gmail.com>
2017-06-07 20:28:15 +0200

    first commit

--- hello.py
+++ hello.py
@@ -1,9 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
+import click;
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='templates/assets')
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html', username= 'Mario')
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+
+@app.route('/assets/<path>')
+def static_file(path):
+    return app.send_static_file(path)
 
 if __name__ == "__main__":
     app.run()
