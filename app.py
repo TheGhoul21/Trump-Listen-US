27eda9384a3ecf83e258dd5f4b7b692763ca029e
Luca Simonetti <little.che90@gmail.com>
2017-06-07 22:09:30 +0200

    fix: nth fix to app.pywq

--- app.py
+++ app.py
@@ -18,5 +18,6 @@ def static_file(path):
     return app.send_static_file(path)
 
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port, debug=True, use_reloader=True)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
